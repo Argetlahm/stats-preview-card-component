f47d71d9a5fed2f0664a32e1a56d694a86c05ffd
Stephen Gunawan Susilo <gunawanstephen@yahoo.com>
2023-11-27 20:35:37 +0700

    fix bug display d:27112023

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App sgs-flex">
       {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -20,24 +20,27 @@ function App() {
       <div className='component sgs-flex'>
         <div className="image-container">
           <ImageComponent class="sgs-fit-container-width header-image" src="images/image-header-mobile.jpg" altName="header-image" />
-          <div class="overlay"></div>
+          <div className="overlay"></div>
+        </div>
+        <div className="info-container">
+          <h1 className="main-title">Get <span className="title-highlight">insights</span> that help your business grow.</h1>
+          <Paragraph class="main-desc" content="Discover the benefits of data analytics and make better decisions regarding revenue, customer 
+          experience, and overall efficiency." />
+          
+          <div className="stats sgs-flex">
+            <div className="stats-item">
+              <div className="number">10k+</div>COMPANIES
+            </div>
+            <div className="stats-item">
+              <div className="number">314</div>TEMPLATES
+            </div>
+            <div className="stats-item">
+              <div className="number">12M+</div>QUERIES
+            </div>
+          </div>
         </div>
         
-        <h1 className="main-title">Get <span className="title-highlight">insights</span> that help your business grow.</h1>
-        <Paragraph class="main-desc" content="Discover the benefits of data analytics and make better decisions regarding revenue, customer 
-        experience, and overall efficiency." />
         
-        <div className="stats sgs-flex">
-          <div className="stats-item">
-            <div className="">10k+</div>companies
-          </div>
-          <div className="stats-item">
-            <div>314</div>templates
-          </div>
-          <div className="stats-item">
-            <div>12m+</div>queries
-          </div>
-        </div>
         
         
       </div>
@@ -59,6 +62,15 @@ function Paragraph (props) {
 
 function ImageComponent (props) {
   return <img className={props.class} src={props.src} alt={props.altName} />
+}
+
+const obj = document.getElementsByClassName("header-image")
+window.addEventListener("resize", detectWindowSize)
+
+function detectWindowSize() {
+  if (window.innerWidth > 1000) {
+    obj[0].setAttribute("src", "images/image-header-desktop.jpg")
+  }
 }
 
 export default App;
